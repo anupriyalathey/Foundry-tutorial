ed58c5c1a517c468f9fb90186c238fff43844582
Anupriya Lathey <anupriyalathey@gmail.com>
2023-07-11 23:55:27 +0530

    added deploy script

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -6,27 +6,30 @@ import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
 contract DeploySimpleStorage is Script {
-    SimpleStorage[] public simpleStorageArray;
-
-    function createSimpleStorageContract() public {
-        SimpleStorage simpleStorage = new SimpleStorage();
-        simpleStorageArray.push(simpleStorage);
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast(); // everything after this line should be sent to rpc
+        SimpleStorage simpleStorage = new SimpleStorage(); // Contract variable = new Contract();
+        vm.stopBroadcast(); // everything before this line should be sent to rpc
+        return simpleStorage;
     }
-
-    function sfStore(
-        uint256 _simpleStorageIndex,
-        uint256 _simpleStorageNumber
-    ) public {
-        // Address
-        // ABI
-        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(
-            _simpleStorageNumber
-        );
-    }
-
-    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
-        return
-            SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]))
-                .retrieve();
-    }
+    // SimpleStorage[] public simpleStorageArray;
+    // function createSimpleStorageContract() public {
+    //     SimpleStorage simpleStorage = new SimpleStorage();
+    //     simpleStorageArray.push(simpleStorage);
+    // }
+    // function sfStore(
+    //     uint256 _simpleStorageIndex,
+    //     uint256 _simpleStorageNumber
+    // ) public {
+    //     // Address
+    //     // ABI
+    //     SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(
+    //         _simpleStorageNumber
+    //     );
+    // }
+    // function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+    //     return
+    //         SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]))
+    //             .retrieve();
+    // }
 }
